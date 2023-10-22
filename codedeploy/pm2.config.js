bac670b6d36b286b415d6a56ac67745c332ad0d0
object-Object <object@objectobject.ca>
2023-10-22 12:47:55 -0400

    oops

--- codedeploy/pm2.config.js
+++ codedeploy/pm2.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     apps: [
         {
-            name:   "HexxyAPI",
-            script: "./scripts/pm2/HexxyAPI.sh",
+            name:         "HexxyAPI",
+            script:       "./scripts/pm2/HexxyAPI.sh",
+            min_uptime:   5,
+            max_restarts: 5,
         },
     ]
 }
