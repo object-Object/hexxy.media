d4435ea9fcca7768c418d2ac05c0af71c33a208e
object-Object <object@objectobject.ca>
2023-10-22 14:36:53 -0400

    Rename pm2 app

--- codedeploy/pm2.config.js
+++ codedeploy/pm2.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps: [
         {
-            name:         "HexxyAPI",
-            script:       "./scripts/pm2/HexxyAPI.sh",
+            name:         "hexxy_media_api",
+            script:       "./scripts/pm2/api.sh",
             min_uptime:   "5s",
             max_restarts: 5,
         },
