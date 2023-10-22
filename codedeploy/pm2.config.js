79823b5173bd62956320b08c2019b512690b1858
object-Object <object@objectobject.ca>
2023-10-22 05:07:01 -0400

    fix pm2 config, again

--- codedeploy/pm2.config.js
+++ codedeploy/pm2.config.js
@@ -2,8 +2,7 @@ module.exports = {
     apps: [
         {
             name:   "HexxyAPI",
-            cwd:    "/var/lib/codedeploy_apps/hexxy.media",
-            script: "source venv/bin/activate && python -m hexxy_media.api.app",
+            script: "venv/bin/python -m hexxy_media.api.app",
         },
     ]
 }
