c451657e0c5e9b77827e8af9efb56f0f5f55b396
object-Object <object@objectobject.ca>
2023-10-22 13:39:49 -0400

    Ignore errors when running application-stop

--- codedeploy/scripts/aws/application-stop.sh
+++ codedeploy/scripts/aws/application-stop.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 cd /var/lib/codedeploy-apps/hexxy.media
 
-sudo su object -c "pm2 --no-color --mini-list delete pm2.config.js"
+sudo su object -c "pm2 --no-color --mini-list delete pm2.config.js || true"
