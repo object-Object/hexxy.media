e9a8463a98229406cde5136907a3fe78f55d335b
object-Object <object@objectobject.ca>
2024-03-02 15:04:18 -0500

    Refactor DNS records and add VS Marketplace verification

--- src/infra/cdktf/stack.py
+++ src/infra/cdktf/stack.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from dataclasses import dataclass
+from typing import Any
 
 import cdktf
 from cdktf_cdktf_provider_cloudflare import provider, record
@@ -17,6 +18,10 @@ class GitHubPagesRecord:
     @property
     def url(self):
         return f"https://{self.subdomain}.hexxy.media"
+
+    @property
+    def name(self):
+        return self.subdomain
 
     @property
     def value(self):
@@ -49,27 +54,72 @@ class HexxyMediaTerraformStack(cdktf.TerraformStack):
             api_token=os.getenv("CLOUDFLARE_API_TOKEN"),
         )
 
-        for record_type, name, value, proxied in [
-            ("A", "*", OBJECTOBJECT_CA, True),
-            ("A", "@", OBJECTOBJECT_CA, True),
-            ("A", "hexxytest", "172.92.208.70", False),
-        ]:
-            record.Record(
-                self,
-                f"{record_type}_{name.replace('*', 'WILDCARD')}_{value}",
-                zone_id=zone_id,
-                type=record_type,
-                name=name,
-                value=value,
-                proxied=proxied,
-            )
+        # simple records
+        for record_type, records in {
+            "A": {
+                "*": (OBJECTOBJECT_CA, True),
+                "@": (OBJECTOBJECT_CA, True),
+                "hexxytest": ("172.92.208.70", False),
+            },
+            "TXT": {
+                "_visual-studio-marketplace-hexdoc-dev.hexxy.media": (
+                    "b5247f90-1e08-4d5c-ac83-558024322149",
+                    False,
+                ),
+            },
+        }.items():
+            for name, (value, proxied) in records.items():
+                create_record(
+                    self,
+                    zone_id=zone_id,
+                    type=record_type,
+                    name=name,
+                    value=value,
+                    proxied=proxied,
+                )
 
+        # GitHub Pages hexdoc books
         for page in github_pages:
             record.Record(
                 self,
-                f"GitHubPages_{page.subdomain}_{page.value}",
+                f"GitHubPages_{page.name}_{page.value}",
                 zone_id=zone_id,
                 type="CNAME",
-                name=page.subdomain,
+                name=page.name,
                 value=page.value,
             )
+
+
+def create_record(
+    scope: Construct,
+    *,
+    zone_id: str,
+    type: str,
+    name: str | None,
+    value: str,
+    priority: int | None = None,
+    proxied: bool = False,
+    **kwargs: Any,
+):
+    match name:
+        case "@":
+            id_parts = [type, "ROOT", value]
+        case "*":
+            id_parts = [type, "WILDCARD", value]
+        case str():
+            id_parts = [type, name, value]
+        case None:
+            id_parts = [type, value]
+            name = "@"
+
+    return record.Record(
+        scope,
+        "_".join(id_parts).replace(".", "-"),
+        zone_id=zone_id,
+        type=type,
+        name=name,
+        value=value,
+        priority=priority,
+        proxied=proxied,
+        **kwargs,
+    )
