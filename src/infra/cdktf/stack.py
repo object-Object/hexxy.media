e6c9e4f1df7569e1f26a144cfbf6781678cb465e
object-Object <object@objectobject.ca>
2024-04-27 20:00:44 -0400

    Prevent registering non-lowercase github usernames since it causes the Pages DNS check to fail

--- src/infra/cdktf/stack.py
+++ src/infra/cdktf/stack.py
@@ -15,6 +15,10 @@ class GitHubPagesRecord:
     subdomain: str
     github_user: str
 
+    def __post_init__(self):
+        if self.github_user != self.github_user.lower():
+            raise ValueError(f"Invalid record {self}: github_user must be lowercase")
+
     @property
     def url(self):
         return f"https://{self.subdomain}.hexxy.media"
