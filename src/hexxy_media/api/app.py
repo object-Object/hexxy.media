c6c7bd054b86829996eaa77b890c31dd64ccb73c
object-Object <object@objectobject.ca>
2024-05-16 22:06:36 -0400

    Change port to 6000

--- src/hexxy_media/api/app.py
+++ src/hexxy_media/api/app.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     uvicorn.run(  # pyright: ignore[reportUnknownMemberType]
         app,
         host="0.0.0.0",
-        port=5000,
+        port=6000,
         # TODO: there's probably a better way to do this...
         root_path="/api/v0",
     )
