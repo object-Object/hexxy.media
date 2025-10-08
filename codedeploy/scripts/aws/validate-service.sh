584441a1c1f4e944b6d097041b0570ec350188d2
object-Object <object@objectobject.ca>
2025-10-08 19:16:07 -0400

    Try to validate a few times before failing

--- codedeploy/scripts/aws/validate-service.sh
+++ codedeploy/scripts/aws/validate-service.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-sleep 10s  # give it time to start up
-curl https://hexxy.media/api/v0/health --write-out "\n" --fail
+for _ in {1..3}; do
+    sleep 15s  # give it time to start up
+    if curl https://hexxy.media/api/v0/health --write-out "\n" --fail; then
+        exit 0
+    fi
+done
+
+exit 1
