f0dfd87b7d9be2f12a1959b715d8063c90e5714a
object-Object <object@objectobject.ca>
2025-10-08 19:08:18 -0400

    Generate constraints file for uv

--- codedeploy/scripts/aws/after-install.sh
+++ codedeploy/scripts/aws/after-install.sh
@@ -6,4 +6,4 @@ cd /var/lib/codedeploy-apps/hexxy.media
 rm -rf venv
 uv venv venv --python 3.11 --seed
 source venv/bin/activate
-uv pip install --find-links dist "hexxy.media[runtime]"
+uv pip install --find-links dist "hexxy.media[runtime]" --constraints dist/requirements.txt
